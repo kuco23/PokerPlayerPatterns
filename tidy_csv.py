f015c91f2992bca8a9b2646f018f3b17cf4e6000
kuco23 <nseverkar@gmail.com>
2020-04-20 16:25:34 +0200

    ocd

--- tidy_csv.py
+++ tidy_csv.py
@@ -1,9 +1,7 @@
 import os
 from pathlib import Path
-
 import pandas as pd
 import numpy as np
-
 from lib import parser
 from lib import TurnId, ActionId
 
@@ -13,15 +11,19 @@ if not os.path.isdir('tidy_data'):
 import_from = Path.cwd() / 'parsed_data'
 export = Path.cwd() / 'tidy_data'
 
-actions = pd.read_csv(import_from / 'playeraction.csv')
-buyins = pd.read_csv(import_from / 'seatjoined.csv')
-received = pd.read_csv(import_from / 'playerreceivedcard.csv')
-blinds = pd.read_csv(import_from / 'playerblind.csv')
-rounds = pd.read_csv(import_from / 'roundid.csv')
-cardshow = pd.read_csv(import_from / 'playershowcards.csv')
-potsize = pd.read_csv(import_from / 'potsize.csv')
-newturn = pd.read_csv(import_from / 'newturn.csv')
-
+varnames = {
+    'actions': 'playeraction',
+    'buyins': 'seatjoined',
+    'received': 'playerreceivedcard',
+    'blinds': 'playerblind',
+    'rounds': 'roundid',
+    'cardshow': 'playershowcards',
+    'potsize': 'potsize',
+    'newturn': 'newturn'
+}
+for varname, filename in varnames.items():
+    path = import_from / (filename + '.csv')
+    globals()[varname] = pd.read_csv(path)
 
 convert_suit = dict(zip(
     ['h', 'd', 'c', 's'],
@@ -31,10 +33,9 @@ convert_value = dict(zip(
     list(map(str, range(2, 11))) + ['J', 'Q', 'K', 'A'],
     list(map(str, range(2, 11))) + ['J', 'Q', 'K', 'A']
 ))
-def encodeCard(card):
-    v = convert_value[card[:-1]]
-    s = convert_suit[card[-1]]
-    return v + s
+encodeCard = lambda card: (
+    convert_value[card[:-1]] + convert_suit[card[-1]]
+)
 
 
 pd.DataFrame(
@@ -146,29 +147,7 @@ action_df = actions.replace([
     user_df, 'inner', 'user'
 ).drop(
     columns=['user']
-)
-# Some players bought in without taking any actions.
-# This will be treated as a fold.
-active_users = set(action_df.user_id.unique())
-inactive_users = [
-    user for user in user_df.user_id
-    if user not in active_users
-]
-inactive_rounds = [
-    buyins_df[
-        buyins_df.user_id == user_id
-    ].round_id.values[0]
-    for user_id in inactive_users
-]
-default_rows = [
-    [0, 0, round_id, 0, -1, user_id]
-    for user_id, round_id in zip(
-        inactive_users, inactive_rounds
-    )
-]
-action_df.append(pd.DataFrame(
-    default_rows, columns = list(action_df.columns)
-)).to_csv(f'{export}/actions.csv', index=False)
+).to_csv(f'{export}/actions.csv', index=False)
 
 
 newturn.new_card = newturn.new_card.replace(pd.NaT, '')
